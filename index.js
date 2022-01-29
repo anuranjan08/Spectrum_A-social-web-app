88c83d4656122ba40cb37169d8fb345733d53063
anuranjan <singh.anuranjan3@gmail.com>
2022-01-30 04:57:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const app=express();
+const port=8000;
+
+app.listen(port,function(err){
+    if(err){
+        console.log(`Error in loading the server :${err}`);
+    }
+    console.log(`Server is running at port: ${port}`)
+
+})
