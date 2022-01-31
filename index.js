035cdb13a24e4aa598e0126d7323e85e8a52c83c
anuranjan <singh.anuranjan3@gmail.com>
2022-01-31 23:46:43 +0530

    added routes and controller for home

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express=require('express');
 const app=express();
 const port=8000;
+//use express router
 
+app.use('/',require('./routes/index'));
 app.listen(port,function(err){
     if(err){
         console.log(`Error in loading the server :${err}`);
