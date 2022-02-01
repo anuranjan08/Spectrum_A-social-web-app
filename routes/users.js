aa8f6a3245b3eb0d67c40ec78343cfb8b890ecfa
anuranjan <singh.anuranjan3@gmail.com>
2022-02-02 00:27:13 +0530

    added routes for sign-up and sign-in

--- routes/users.js
+++ routes/users.js
@@ -7,5 +7,8 @@ router.get('/profile',usersController.profile);
 
 router.get('/posts',postsController.posts);
 
+router.get('/sign-up',usersController.signUp);
+
+router.get('/sign-in',usersController.signIn); 
 
 module.exports=router;
