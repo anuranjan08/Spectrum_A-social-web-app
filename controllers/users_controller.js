1f65a4b18e9029e218f847809d45c73a4abed91c
anuranjan <singh.anuranjan3@gmail.com>
2022-02-02 00:28:56 +0530

    added action for sign-up and sign-in routes

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,25 @@
 module.exports.profile=function(req,res){
     return res.end('<h1> User Profile </h1>')
 }
+
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"Spectrum | Sign Up"
+    })
+}
+
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"Spectrum | Sign In"
+    })   
+}
+
+//get the sign up data
+module.exports.create=function(req,res){
+    ///
+}
+//create the session 
+module.exports.createSession=function(req,res){
+
+}
+
