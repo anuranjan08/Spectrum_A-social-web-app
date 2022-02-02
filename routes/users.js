74fd1d2ce3a44abc5f46080c96e0dc5b72ccd23b
anuranjan <singh.anuranjan3@gmail.com>
2022-02-03 02:48:12 +0530

    added create session route

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn); 
 router.post('/create',usersController.create); 
 
-//router.get('/create-session',usersController.createSession); 
+router.post('/create-session',usersController.createSession); 
 
 
 module.exports=router;
